5ce308cc88a48c9b30d0e672a61d1c2ae5bdac7d
Khai Bui <khai_bui_quoc@bita.jp>
2023-04-10 11:56:30 +0700

    feat: add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['stylelint-config-recommended', 'stylelint-config-prettier'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'no-invalid-position-at-import-rule': null,
+  },
+}
