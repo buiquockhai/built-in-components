9c07f3371b5e03355908f3bf0d916d0cd22ea248
Khai Bui <khai_bui_quoc@bita.jp>
2023-04-11 11:43:18 +0700

    feat: intro image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,13 @@ export default function IndexPage() {
   return (
     <div className='relative'>
       <Opening />
+      <div>
+        The will-change CSS property hints to browsers how an element is
+        expected to change. Browsers may set up optimizations before an element
+        is actually changed. These kinds of optimizations can increase the
+        responsiveness of a page by doing potentially expensive work before they
+        are actually required.
+      </div>
     </div>
   )
 }
